d31f22025d7515dc155cd00640666b538026518f
tdilauro <tim.dilauro@gmail.com>
2016-08-09 17:57:03 -0400

    handle multiple arguments in 'ls' command

--- lib/fixi/command/ls.rb
+++ lib/fixi/command/ls.rb
@@ -33,39 +33,40 @@ class Fixi::Command::Ls
       opt :verbose, "Include all information known about each file. By default,
         only paths will be listed.".pack
     end
-    # todo: exception if multiple args OR handle multiple args
     # todo:less brittle way to generate JSON (and maybe don't pretty print)
-    index = Fixi::Index.new(args[0])
     if opts[:json]
       print "["
     end
     i = 0
-    index.each(args[0], opts) do |hash|
-      path = hash['relpath']
-      path = index.rootpath + '/' + path if opts[:absolute]
-      if opts[:verbose]
-        print "size=#{hash['size']},mtime=#{Time.at(hash['mtime']).utc.iso8601}"
-        print ",md5=#{hash['md5']}" if hash['md5']
-        print ",sha1=#{hash['sha1']}" if hash['sha1']
-        print ",sha256=#{hash['sha256']}" if hash['sha256']
-        print ",sha384=#{hash['sha384']}" if hash['sha384']
-        print ",sha512=#{hash['sha512']}" if hash['sha512']
-        puts " #{path}"
-      elsif opts[:json]
-        print "," if i > 0
-        puts "\n  { path: \"#{path}\","
-        puts "    size: \"#{hash['size']}\","
-        print "    mtime: \"#{Time.at(hash['mtime']).utc.iso8601}\""
-        print ",\n    md5: \"#{hash['md5']}\"" if hash['md5']
-        print ",\n    sha1: \"#{hash['sha1']}\"" if hash['md5']
-        print ",\n    sha256: \"#{hash['sha256']}\"" if hash['sha256']
-        print ",\n    sha384: \"#{hash['sha384']}\"" if hash['sha384']
-        print ",\n    sha512: \"#{hash['sha512']}\"" if hash['sha512']
-        print " }"
-      else
-        puts path
+    args.each do |path|
+      index = Fixi::Index.new(path)
+      index.each(path, opts) do |hash|
+        path = hash['relpath']
+        path = index.rootpath + '/' + path if opts[:absolute]
+        if opts[:verbose]
+          print "size=#{hash['size']},mtime=#{Time.at(hash['mtime']).utc.iso8601}"
+          print ",md5=#{hash['md5']}" if hash['md5']
+          print ",sha1=#{hash['sha1']}" if hash['sha1']
+          print ",sha256=#{hash['sha256']}" if hash['sha256']
+          print ",sha384=#{hash['sha384']}" if hash['sha384']
+          print ",sha512=#{hash['sha512']}" if hash['sha512']
+          puts " #{path}"
+        elsif opts[:json]
+          print "," if i > 0
+          puts "\n  { path: \"#{path}\","
+          puts "    size: \"#{hash['size']}\","
+          print "    mtime: \"#{Time.at(hash['mtime']).utc.iso8601}\""
+          print ",\n    md5: \"#{hash['md5']}\"" if hash['md5']
+          print ",\n    sha1: \"#{hash['sha1']}\"" if hash['md5']
+          print ",\n    sha256: \"#{hash['sha256']}\"" if hash['sha256']
+          print ",\n    sha384: \"#{hash['sha384']}\"" if hash['sha384']
+          print ",\n    sha512: \"#{hash['sha512']}\"" if hash['sha512']
+          print " }"
+        else
+          puts path
+        end
+        i += 1
       end
-      i += 1
     end
     if opts[:json]
       print "\n" if i > 0
